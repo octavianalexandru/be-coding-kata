26e35452c965a7715777f5e63509a015f703082a
Octavian Alexandru <octavianalexandru@Octavians-MacBook-Pro.local>
2025-01-21 14:51:44 +0200

    HAILO-1: Initialize app

---
+++ src/main/java/com/hailo_demo/be_coding_kata/BeCodingKataApplication.java
@@ -0,0 +1,13 @@
+package com.hailo_demo.be_coding_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCodingKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCodingKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hailo_demo/be_coding_kata/BeCodingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hailo_demo.be_coding_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCodingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
