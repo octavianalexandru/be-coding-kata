9e9ac35f376f819b06b071a68f92850262b8dff2
Octavian Alexandru <octavianalexandru@Octavians-MacBook-Pro.local>
2025-01-21 17:00:32 +0200

    HAIILO-1: Refactored and moved pricing data load to a separate component that resembles a repo

---
+++ src/main/java/com/hailo_demo/be_coding_kata/repository/PricingDataRepository.java
@@ -0,0 +1,65 @@
+package com.hailo_demo.be_coding_kata.repository;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hailo_demo.be_coding_kata.domain.Offer;
+import com.hailo_demo.be_coding_kata.domain.ProductPricingData;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class PricingDataRepository {
+    private final ObjectMapper objectMapper;
+    private final Resource pricingTableResource;
+    private final Map<String, ProductPricingData> productPriceMap = new HashMap<>();
+
+
+    public PricingDataRepository(ObjectMapper objectMapper, Resource pricingTableResource) {
+        this.objectMapper = objectMapper;
+        this.pricingTableResource = pricingTableResource;
+        loadPricingDataFromJson();
+    }
+
+    private void loadPricingDataFromJson() {
+        try {
+            File file = pricingTableResource.getFile();
+            JsonNode productsNode = objectMapper.readTree(file);
+            for (JsonNode product : productsNode) {
+                String barcode = product.get("barcode").asText();
+                String name = product.get("name").asText();
+                double priceForOne = product.get("priceForOne").asDouble();
+                JsonNode offerNode = product.get("offer");
+
+                Offer offer = null;
+                if(!offerNode.isNull()){
+                    Integer numberOfItemsForOffer = offerNode.get("numberOfItemsForOffer").asInt();
+                    Double sumOfItemsOffer = offerNode.get("sumOfItemsOffer").asDouble();
+                    offer = Offer.builder().
+                            numberOfItemsForOffer(numberOfItemsForOffer).
+                            sumOfItemsOffer(sumOfItemsOffer).
+                            build();
+                }
+
+                ProductPricingData productPricingData = ProductPricingData.builder().
+                        name(name).
+                        priceForOne(priceForOne)
+                        .offer(offer)
+                        .build();
+
+                productPriceMap.put(barcode, productPricingData);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load pricing data", e);
+        }
+    }
+
+    public Map<String, ProductPricingData> getProductPriceMap() {
+        return productPriceMap;
+    }
+}
--- src/main/java/com/hailo_demo/be_coding_kata/service/CheckoutService.java
+++ src/main/java/com/hailo_demo/be_coding_kata/service/CheckoutService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hailo_demo.be_coding_kata.controller.CheckoutController;
 import com.hailo_demo.be_coding_kata.domain.Offer;
 import com.hailo_demo.be_coding_kata.domain.ProductPricingData;
+import com.hailo_demo.be_coding_kata.repository.PricingDataRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -20,59 +21,11 @@ import java.util.Map;
 
 @Slf4j
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
+    private final PricingDataRepository pricingDataRepository;
 
-
-    private final Resource pricingTableResource;
-    private final ObjectMapper objectMapper;
-
-    private Map<String, ProductPricingData> productPriceMap = new HashMap<>();
-
-
-    @PostConstruct
-    public void init() {
-        try {
-            log.info("Pricing table file path: {}", pricingTableResource.getFile().getAbsolutePath());
-            loadPricingTable();
-        } catch (IOException e) {
-            log.error("Failed to load pricing table from JSON file.", e);
-        }
+    public ProductPricingData getProductPricing(String barcode) {
+        return pricingDataRepository.getProductPriceMap().get(barcode);
     }
-
-    private void loadPricingTable() throws IOException {
-        File pricingTableFile = pricingTableResource.getFile();
-        JsonNode productsNode = objectMapper.readTree(pricingTableFile);
-
-        for (JsonNode product : productsNode) {
-            String barcode = product.get("barcode").asText();
-            String name = product.get("name").asText();
-            double priceForOne = product.get("priceForOne").asDouble();
-            JsonNode offerNode = product.get("offer");
-
-            Offer offer = null;
-            if(!offerNode.isNull()){
-                Integer numberOfItemsForOffer = offerNode.get("numberOfItemsForOffer").asInt();
-                Double sumOfItemsOffer = offerNode.get("sumOfItemsOffer").asDouble();
-                offer = Offer.builder().
-                        numberOfItemsForOffer(numberOfItemsForOffer).
-                        sumOfItemsOffer(sumOfItemsOffer).
-                        build();
-            }
-
-            ProductPricingData productPricingData = ProductPricingData.builder().
-                    name(name).
-                    priceForOne(priceForOne)
-                    .offer(offer)
-                    .build();
-
-            productPriceMap.put(barcode, productPricingData);
-        }
-
-        log.info("Product prices loaded successfully: {}", productPriceMap);
-    }
-
-//    public Double calculateProductSum(String[] barCodes){
-//
-//    }
 }
